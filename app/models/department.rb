bcd75abd9cc21f39ddfa4ec509582e001e47c985
aromalsanhosh <aromalsanthosh@protonmail.com>
2023-05-25 00:57:19 +0530

    update : Modify View

--- app/models/department.rb
+++ app/models/department.rb
@@ -1,3 +1,11 @@
+# Schema Information
+# Table: departments
+
+# id: bigint, not null, primary key
+# name: varchar(255)
+# created_at: datetime, not null
+# updated_at: datetime, not null
+
 class Department < ApplicationRecord
     has_many :employees
 end
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,3 +1,16 @@
+# Schema Information
+# Table: employees
+
+# id: bigint, not null, primary key
+# first_name: varchar(255)
+# last_name: varchar(255)
+# email_address: varchar(255)
+# phone: varchar(255)
+# salary: float
+# department_id: bigint
+# created_at: datetime, not null
+# updated_at: datetime, not null
+
 class Employee < ApplicationRecord
   belongs_to :department
 end
